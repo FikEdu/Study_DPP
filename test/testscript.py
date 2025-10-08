9bf927ead55cca491738b37a197b194afafb1291
FikEdu <nikodem.fik@edu.uekat.pl>
2025-10-08 11:27:31 +0200

    added fibonacci

--- test/testscript.py
+++ test/testscript.py
@@ -1,7 +1,17 @@
-def is_palindrome(word):
-    drow = ""
-    for i in range(len(word)):
-        drow += word[len(word)-i-1]
-    return word == drow
+#def is_palindrome(word):
+#    drow = ""
+#    for i in range(len(word)):
+#        drow += word[len(word)-i-1]
+#    return word == drow
+#
+#print(is_palindrome("skala"))
 
-print(is_palindrome("skala"))
+def fibonacci(liczba):
+    if liczba == 1:
+        return 1
+    elif liczba == 0:
+        return 0
+    elif liczba > 1:
+        return fibonacci(liczba-1) + fibonacci(liczba-2)
+
+print(fibonacci(20))
