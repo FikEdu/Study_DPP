5bebfdef9515b5fb4f60dc3b7e23e495f7e6999f
FikEdu <nikodem.fik@edu.uekat.pl>
2025-10-22 11:03:54 +0200

    added CSV to SQL database #86c63yx0v #86c63yx1y

--- python/script.py
+++ python/script.py
@@ -1,30 +1,62 @@
+import sqlite3
 import csv
-import uvicorn
-from fastapi import FastAPI
+con = sqlite3.connect('movies.db')
 
-app = FastAPI()
+cur = con.cursor()
 
-class Movie:
-    def __init__(self, id, title, genres):
-        self.id = id
-        self.title = title
-        self.genres = genres
+# movies
+if cur.execute("SELECT name FROM sqlite_master WHERE name='movies'").fetchone() is None:
+    cur.execute('''CREATE TABLE movies(mID, Title, Genres)''')
 
-@app.get("/")
-def read_root():
-    return {"Hello" : "World"}
-
-@app.get("/movies")
-def read_item():
-    return Movies
-
-file = open("database/movies.csv", "r", encoding="utf-8")
+file = open('database/movies.csv', 'r', encoding='utf-8')
 reader = csv.reader(file)
-Movies = []
-for line in reader:
-    if line[0].isdigit():
-        Movies.append(Movie(line[0], line[1], line[2]).__dict__)
+data = []
+for row in reader:
+    data.append(row)
+if cur.execute('''select count(*) from movies''').fetchone()[0]>0:
+    cur.executemany('''Insert into movies(mID, Title, Genres) values (?,?,?)''', data)
+    con.commit()
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+### links
+if cur.execute("SELECT name FROM sqlite_master WHERE name='links'").fetchone() is None:
+    cur.execute('''CREATE TABLE links(mID, imdbID, tmdbID)''')
+
+file = open('database/links.csv', 'r', encoding='utf-8')
+reader = csv.reader(file)
+data = []
+for row in reader:
+    data.append(row)
+if cur.execute('''select count(*) from links''').fetchone()[0]>0:
+    cur.executemany('''Insert into links(mID, imdbID, tmdbID) values (?,?,?)''', data)
+    con.commit()
+
+#ratings
+
+if cur.execute("SELECT name FROM sqlite_master WHERE name='ratings'").fetchone() is None:
+    cur.execute('''CREATE TABLE ratings(uID, mID, rating, timestamp)''')
+
+file = open('database/ratings.csv', 'r', encoding='utf-8')
+reader = csv.reader(file)
+data = []
+for row in reader:
+    data.append(row)
+if cur.execute('''select count(*) from ratings''').fetchone()[0]>0:
+    cur.executemany('''Insert into ratings(uID, mID, rating, timestamp) values (?,?,?,?)''', data)
+    con.commit()
+
+#tags
+
+if cur.execute("SELECT name FROM sqlite_master WHERE name='tags'").fetchone() is None:
+    cur.execute('''CREATE TABLE tags(uID, mID,tag, timestamp)''')
+
+file = open('database/tags.csv', 'r', encoding='utf-8')
+reader = csv.reader(file)
+data = []
+for row in reader:
+    data.append(row)
+if cur.execute('''select count(*) from tags''').fetchone()[0]>0:
+    cur.executemany('''Insert into tags(uID, mID,tag, timestamp) values (?,?,?,?)''', data)
+    con.commit()
+
+con.close()
