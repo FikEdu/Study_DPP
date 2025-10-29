a7989bcf4691b11fdb98c027da12342c711aa0c7
FikEdu <nikodem.fik@edu.uekat.pl>
2025-10-29 09:00:18 +0100

    added api for movies to database file #86c63yx26

--- python/script.py
+++ python/script.py
@@ -1,5 +1,7 @@
 import sqlite3
 import csv
+import uvicorn
+from fastapi import FastAPI
 con = sqlite3.connect('movies.db')
 
 cur = con.cursor()
@@ -51,5 +53,33 @@ if cur.execute("SELECT name FROM sqlite_master WHERE name='tags'").fetchone() is
         data.append(row)
     cur.executemany('''Insert into tags(uID, mID,tag, timestamp) values (?,?,?,?)''', data)
     con.commit()
+
+app = FastAPI()
+
+class Movie:
+    def __init__(self, id, title, genres):
+        self.id = id
+        self.title = title
+        self.genres = genres
+
+@app.get("/")
+def read_root():
+    return {"Hello" : "World"}
+
+@app.get("/movies")
+def read_item():
+    return Movies
+
+file = open("database/movies.csv", "r", encoding="utf-8")
+query = [list(row) for row in cur.execute('''SELECT * FROM movies''').fetchall()]
+Movies = []
+for line in query:
+    if line[0].isdigit():
+        Movies.append(Movie(line[0], line[1], line[2]).__dict__)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
+
 
 con.close()
